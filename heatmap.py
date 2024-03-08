f2e44c9c7f77ea3149273c610fd6c78513b264d8
Daniel Coombe <deadtomgc@gmail.com>
2024-03-07 22:19:11 -0500

    uses google API

--- heatmap.py
+++ heatmap.py
@@ -5,29 +5,65 @@ from folium.plugins import HeatMap
 import time
 import openpyxl
 
+
+import requests
+
+def geolocate_address(address, api_key):
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json?"
+    params = {
+        'address': address,
+        'key': api_key
+    }
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data['status'] == 'OK':
+            # Extract latitude and longitude
+            latitude = data['results'][0]['geometry']['location']['lat']
+            longitude = data['results'][0]['geometry']['location']['lng']
+            return latitude, longitude
+        else:
+            return None, None
+    else:
+        print(f"HTTP error {response.status_code}")
+        return None, None
+
+
+
 # Load the addresses from a csv file
 df = pd.read_excel('data.xlsx', engine="openpyxl")
-geolocator = Nominatim(user_agent="Heritage_Valley_Research")
+#geolocator = Nominatim(user_agent="Heritage_Valley_Research")
+api_key = open('API_token.txt').readline()
+
 
 # Create an empty list to hold the geocoded coordinates
 location_list = []
+not_found_list = []
 
 for index, row in df.iterrows():
     try:
         address = row['Address']
-        location = geolocator.geocode(address)
+        if isinstance(address, str):
+            latitude, longitude = geolocate_address(address, api_key)
         
-        if location:
-            location_list.append([location.latitude, location.longitude])
-        else:
-            print("Failed to locate: " + address)
+            if latitude is not None and longitude is not None:
+                location_list.append([latitude, longitude])
+                print("Located: " + address + " at " + str(latitude) + " , " + str(longitude))
+            else:
+                print("Failed to locate: " + address)
+                not_found_list.append(address)
+            time.sleep(0.1)
     except Exception as e:
         print(f"Error with row {index}: {e}")
+        not_found_list.append("Error with row {index}: {e}")
         
-    time.sleep(1)
+    
 
 file = open("found_locations.txt",'w')
 file.write(str(location_list))
+file.close()
+file = open("not_found_locations.txt",'w')
+file.write(str(not_found_list))
 file.close()
 
 print(location_list)
